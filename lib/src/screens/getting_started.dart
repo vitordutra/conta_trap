90bc5d2e4c946b747a33366b821d7329d6480e2d
Vitor Dutra <jo.vitor.d@gmail.com>
2022-01-09 17:43:30 -0300

    Add Hello World

--- lib/src/screens/getting_started.dart
+++ lib/src/screens/getting_started.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class GettingStarted extends StatelessWidget {
-  const GettingStarted({ Key? key }) : super(key: key);
+  const GettingStarted({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-
+      child: Text("Hello World"),
     );
   }
 }
